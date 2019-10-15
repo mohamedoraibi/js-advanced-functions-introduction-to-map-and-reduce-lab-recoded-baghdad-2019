4f1e450376e665edb18e57116b494108577260f0
Mohammed Al-Oraibi <mohamed.b.oraibi@gmail.com>
2019-10-15 13:31:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+
+function mapToNegativize(sourceArray){
+  let arrays =[]
+  arrays = sourceArray.map(x=>x*-1);
+  return arrays
+}
+
+
+function mapToNoChange(sourceArray){
+  let arrays =[]
+  return arrays
+  
+}
+function mapToDouble(sourceArray){
+  let arrays =[]
+  return arrays
+}
+function mapToSquare(sourceArray){
+  let arrays =[]
+  return arrays
+}
+
+
+function reduceToTotal(sourceArray, startingPoint){
+  let arrays =[]
+  return arrays
+}
+function reduceToAllTrue(sourceArray){
+  let arrays =[]
+  
+  arrays =sourceArray.reduce((a, b) => a + b, 0)
+  return arrays
+}
+function reduceToAnyTrue(sourceArray){
+  let arrays =[]
+  return arrays
+}
