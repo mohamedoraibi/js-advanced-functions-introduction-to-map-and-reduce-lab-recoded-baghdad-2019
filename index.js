7ecb56b870e9094bf59362d5efaa15812b8fd622
Mohammed Al-Oraibi <mohamed.b.oraibi@gmail.com>
2019-10-16 05:35:53 +0000

    Done.

--- index.js
+++ index.js
@@ -3,36 +3,42 @@
 function mapToNegativize(sourceArray){
   let arrays =[]
   arrays = sourceArray.map(x=>x*-1);
+  
   return arrays
 }
 
 
 function mapToNoChange(sourceArray){
-  let arrays =[]
-  return arrays
+    return sourceArray
   
 }
 function mapToDouble(sourceArray){
   let arrays =[]
+  arrays = sourceArray.map(x=>x*2);
   return arrays
 }
 function mapToSquare(sourceArray){
   let arrays =[]
+  arrays = sourceArray.map(x=>x*x);
   return arrays
 }
 
 
-function reduceToTotal(sourceArray, startingPoint){
+function reduceToTotal(sourceArray,startingPoint=0){
   let arrays =[]
+  
+  arrays =sourceArray.reduce((a, b) => a + b, startingPoint)
   return arrays
 }
 function reduceToAllTrue(sourceArray){
   let arrays =[]
   
-  arrays =sourceArray.reduce((a, b) => a + b, 0)
+  arrays =sourceArray.reduce((a, b) => a && Boolean(b), true)
   return arrays
 }
 function reduceToAnyTrue(sourceArray){
   let arrays =[]
+  
+  arrays =sourceArray.reduce((a, b) => a || Boolean(b), false)
   return arrays
 }
